40eb52a9bb9072c80d17b1d29995a4864857f4c5
Asavani <39687038+Asavani@users.noreply.github.com>
2020-12-15 17:43:53 +0530

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "";
+            Console.WriteLine("Enter Text:");
+            msg = Console.ReadLine();
+            cal_char(msg);
+        }
+        static void  cal_char(string msg)
+        {
+            if(msg.Length>0)
+            {
+                
+                
+                int k = 0;
+                for (int i = 0; i < msg.Length; i++)
+                {
+                    k = 0;
+                    for(int j=0;j<msg.Length;j++)
+                    {
+                        if(Char.ToLower(msg[i]).Equals(Char.ToLower(msg[j])))
+                        {
+                            k++;
+                        }
+                
+                    }
+                    if(Char.ToLower(msg[i]).Equals(' '))
+                    {
+                        Console.WriteLine("White Space Repeated:"+k);
+                    }
+                    else
+                    {
+                        Console.WriteLine(msg[i] + " Repeated=>" + k);
+                    }
+                    
+                    
+
+                }
+            }
+            
+        }
+    }
+}
